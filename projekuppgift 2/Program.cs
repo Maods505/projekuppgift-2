9a8639c54948d480887fcfa7e87d0cc5af617d7c
abdullahmuhammad1 <89831979+abdullahmuhammad1@users.noreply.github.com>
2021-11-23 09:00:41 +0100

    Update Program.cs

--- projekuppgift 2/Program.cs
+++ projekuppgift 2/Program.cs
@@ -54,3 +54,78 @@ namespace projekuppgift_2
         }
     }
 }
+
+using System;
+using System.IO;
+
+namespace projekuppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string menyval = "0";
+            while (menyval != "5")
+            { 
+
+                Console.WriteLine("välj ett av följande alternativ");
+                Console.WriteLine("1. visa topplistan");
+                Console.WriteLine("2. ändra på topplistan");
+                Console.WriteLine("3. visa hela topplistan");
+                Console.WriteLine("4. söka efter ett låtnamn");
+                Console.WriteLine("5. avsluta programmet");
+
+                string användarensval = "";
+                användarensval = Console.ReadLine();
+
+                string[] låtar = new string[20];
+
+                for (int i = 0; i < låtar.Length; i++)
+                {
+                    låtar[i] = "tom";
+                }
+
+
+                switch (användarensval)
+                {
+                    case "1":
+                        Console.WriteLine($"här är alla låtar som finns inskrivna ");
+                        for (int i = 0; i < låtar.Length; i++)
+                        {
+                            Console.WriteLine(låtar[i]);
+                        }
+                        break;
+
+
+                    case "2":
+                        for (int i = 0; i < låtar.Length; i++)
+                        {
+                            Console.WriteLine($"skriv in låt number {i + 1}");
+                            låtar[i] = Console.ReadLine();
+
+                        }
+
+
+
+                        break;
+
+
+
+
+                    case "4":
+
+
+                        break;
+
+                }
+            }
+
+            
+            
+
+            
+
+        }
+    }
+}
