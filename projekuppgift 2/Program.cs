6e4d53fb0f8396e55de4fb535df8422fbba05aeb
Maods505 <89831889+Maods505@users.noreply.github.com>
2021-11-30 08:13:34 +0100

    Update Program.cs

--- projekuppgift 2/Program.cs
+++ projekuppgift 2/Program.cs
@@ -1,60 +1,3 @@
-﻿using System;
-using System.IO;
-
-namespace projekuppgift_2
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-
-            Console.WriteLine("välj ett av följande alternativ");
-            Console.WriteLine("1. visa topplistan");
-            Console.WriteLine("2. ändra på topplistan");
-            Console.WriteLine("3. visa hela topplistan");
-            Console.WriteLine("4. söka efter ett låtnamn");
-            Console.WriteLine("5. avsluta programmet");
-
-            string användarensval = "";
-            användarensval = Console.ReadLine();
-
-            string[] låtar = new string[20];
-
-            for (int i = 0; i < låtar.Length; i++)
-            {
-                låtar[i] = "tom";
-            }
-            
-
-            switch (användarensval)
-            {
-                case "1":
-                    Console.WriteLine($"här är alla låtar som finns inskrivna ");
-                    for (int i = 0; i < låtar.Length; i++)
-                    {
-                        Console.WriteLine(låtar[i]);
-                    }
-                    break;
-
-
-                case "2":
-                    for (int i = 0; i < låtar.Length; i++)
-                    {
-                        Console.WriteLine($"skriv in låt number {i + 1}");
-                        låtar[i] = Console.ReadLine();
-                    }
-                    break;
-            }
-
-            
-            
-
-            
-
-        }
-    }
-}
-
 using System;
 using System.IO;
 
@@ -65,27 +8,28 @@ namespace projekuppgift_2
         static void Main(string[] args)
         {
 
+            string[] låtar = new string[20];
+
+            for (int i = 0; i < låtar.Length; i++)
+            {
+                låtar[i] = "tom";
+            }
+
             string menyval = "0";
             while (menyval != "5")
-            { 
+            {
 
                 Console.WriteLine("välj ett av följande alternativ");
                 Console.WriteLine("1. visa topplistan");
                 Console.WriteLine("2. ändra på topplistan");
-                Console.WriteLine("3. visa hela topplistan");
+                Console.WriteLine("3. Ändra på en Låt i topplistan");
                 Console.WriteLine("4. söka efter ett låtnamn");
                 Console.WriteLine("5. avsluta programmet");
 
                 string användarensval = "";
                 användarensval = Console.ReadLine();
 
-                string[] låtar = new string[20];
-
-                for (int i = 0; i < låtar.Length; i++)
-                {
-                    låtar[i] = "tom";
-                }
-
+                
 
                 switch (användarensval)
                 {
@@ -106,12 +50,18 @@ namespace projekuppgift_2
 
                         }
 
-
-
                         break;
 
 
 
+                    case "3":
+                        {
+                            Console.WriteLine("Ändra en låt i din lista. Välj ett nummer i din topplista.");
+                            int LåtVal = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Vad vill du ändra låten till?");
+                            låtar[LåtVal - 1] = Console.ReadLine();
+                        }
+                        break;
 
                     case "4":
 
@@ -121,10 +71,10 @@ namespace projekuppgift_2
                 }
             }
 
-            
-            
 
-            
+
+
+
 
         }
     }
